204222240f30723d28d6a39df7290eb37c3d480a
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-15 21:09:51 +0100

    Update and rename affinecipher to affinecipher.php

--- src/cipher/affinecipher.php
+++ src/cipher/affinecipher.php
@@ -4,13 +4,17 @@ namespace cipher;
 
 class affinecipher extends cipher {
   
-    protected $a, $b;
+    protected $a, $b, $ainv;
 
     function __construct ($alphabet, $a, $b) {
-      parent::__construct ($alphabet)
-      $this->alphabet = $alphabet;
+      parent::__construct ($alphabet);
+      $this->setab ($a, $b);
+    }
+    
+    function setab ($a, $b) {
       $this->a = $a; 
       $this->b = $b;
+      $ainv = $this->modinvers ($this->a, strlen($this->alphabet));
     }
     
     function modinvers ($a, $m) {
@@ -24,7 +28,7 @@ class affinecipher extends cipher {
     
     function encode ($msg) {
         $s = "";
-          for ($i=0; $i < strlen($msg); $i++) {
+        for ($i=0; $i < strlen($msg); $i++) {
             $pos = strpos ($this->alphabet, $msg[$i]);
             if ($pos !== FALSE) {
                 $s .= $this->alphabet [($pos * $this->a + $this->b) % strlen($this->alphabet)];
@@ -36,7 +40,6 @@ class affinecipher extends cipher {
     
     function decode ($msg) {
         
-        $ainv = $this->modinvers ($this->a, strlen($this->alphabet));
         if ($ainv == 0) return "Error decoding as a and size of the alphabet are not coprime";
         $s = "";
         for ($i=0; $i < strlen($msg); $i++) {
