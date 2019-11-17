419d875cef2a84c9cad75b182b70a1464fb19551
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-17 16:36:07 +0100

    Update beaufortcipher.php

--- src/cipher/polyalphabeticcipher/vigenerecipher/beaufortcipher.php
+++ src/cipher/polyalphabeticcipher/vigenerecipher/beaufortcipher.php
@@ -6,9 +6,31 @@ class beaufortcipher extends \cipher\polyalphabeticcipher\vigenerecipher {
 
   public function settableau ($keyiterator="", $keycipher="", $keyplain="", $keycol="") {
 
-          $this->alphabet    = strrev ($this->alphabet);
-          parent::settableau ($keyiterator, $keycipher, $keyplain, $keycol);
-          $this->alphabet    = strrev ($this->alphabet);
+        $this->keyiterator = $keyiterator;
+        $this->iterkeylen  = strlen ($keyiterator);
+        $this->keycipher   = $keycipher;
+        $this->keyplain    = $keyplain;
+        $this->keycol      = $keycol;
+        
+        // Create plainalphabet
+        $this->plainalphabet = $this->shufflealphabet ($keyplain, $this->alphabet);
+        
+        // Create cipheralphabet
+        $this->cipheralphabet = $this->shufflealphabet ($keycipher, $this->alphabet);
+        
+        // Create a tableau - each row contains a shifted alphabet
+        $this->tableau = array();
+        if ($keycol == "")
+            $pos = 0;
+            else {
+                $pos = $this->strpos2($this->plainalphabet, $keycol);
+                if ($pos === FALSE) $pos=0;
+            }
+            $s = $this->cipheralphabet;
+            for ($i=0; $i<strlen($this->cipheralphabet); $i++) {
+                $this->tableau[$this->cipheralphabet[($i + $pos) % strlen($this->cipheralphabet) ]]= strrev($s);
+                $s = substr($s,1) . $s[0];
+            }
           
   }
   
