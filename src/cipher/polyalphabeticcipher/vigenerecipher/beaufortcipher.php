01e461abe480751ef8af0de2e75d9ddf17c63364
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-17 16:18:54 +0100

    Update beaufortcipher.php
    Fixed class extend; simplified settableau using strrev

--- src/cipher/polyalphabeticcipher/vigenerecipher/beaufortcipher.php
+++ src/cipher/polyalphabeticcipher/vigenerecipher/beaufortcipher.php
@@ -2,38 +2,15 @@
 
 namespace cipher\polyalphabeticcipher\vigenerecipher;
 
-class beaufortcipher extends vigenerecipher {
+class beaufortcipher extends \cipher\polyalphabeticcipher\vigenerecipher {
 
   public function settableau ($keyiterator="", $keycipher="", $keyplain="", $keycol="") {
 
-          $this->keyiterator = $keyiterator;
-          $this->iterkeylen  = strlen ($keyiterator);
-          $this->keycipher   = $keycipher;
-          $this->keyplain    = $keyplain;
-          $this->keycol      = $keycol;
-
-          // Create plainalphabet
-          $this->plainalphabet = $this->shufflealphabet ($keyplain, $this->alphabet);
-
-          // Create cipheralphabet
-          $this->cipheralphabet = $this->shufflealphabet ($keycipher, $this->alphabet);
-
-          // Create a tableau - each row contains a shifted alphabet
-          $this->tableau = array();
-          if ($keycol == "")
-              $pos = 0;
-          else {
-              $pos = $this->strpos2($this->plainalphabet, $keycol);
-              if ($pos === FALSE) $pos=0;
-          }
-          
-          // The beaufort cipher uses a reversed alphabet as a tableau
-          $s = strrev($this->cipheralphabet);
-          for ($i=0; $i<strlen($this->cipheralphabet); $i++) {
-              $this->tableau[$this->cipheralphabet[($i + $pos) % strlen($this->cipheralphabet) ]]= $s;
-              $s = substr($s,1) . $s[0];
-          }
-      }
+          $this->alphabet    = strrev ($this->alphabet);
+          parent::settableau ($keyiterator, $keycipher, $keyplain, $keycol);
+          $this->alphabet    = strrev ($this->alphabet);
+  }
+  
 }
 
 ?>
