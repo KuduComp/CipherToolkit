09def05fc58a784f94e5ac4dd9d0dbf193d8a9db
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-18 16:57:06 +0100

    Update amscocipher.php

--- src/cipher/amscocipher.php
+++ src/cipher/amscocipher.php
@@ -1,30 +1,87 @@
 <?php
 
 
-	class amscocipher extends cipher {
+class amscocipher extends cipher {
 
-		protected $numkey;
-		protected $numkeymap;
+	protected $numkey;
+	protected $numkeymap;
 
-		function __construct ($alphabet, $numkey = 0) {
-			parent::__construct ($alphabet);
-			$this-setnumkey ($numkey);
+	function __construct ($alphabet, $numkey = 0) {
+		parent::__construct ($alphabet);
+		$this-setnumkey ($numkey);
+	}
+
+	function setnumkey ($numkey) {
+		$this->numkey = $numkey;
+		$tmparr = str_split(strval($numkey));
+		$numkeymap = array();
+		foreach ($tmparr as $t) $numkeymap[] = intval($t);
+	}
+
+	function encodecolumnartransposition ($msg, $key) {
+
+	    $ncol = sizeof($key);
+	    $row = 0;
+	    $col = 0;
+	    $table = array();
+
+	    // Write message row after row in array table
+	    for ($i = 0; $i < strlen($msg); $i++) {
+		$table[$col][$row] = $msg[$i];
+		if ($col < ($ncol-1)) 
+			$col++;
+		else if ($i < (strlen($msg)-1)) {
+			$col = 0;
+			$row++;
 		}
+	    }
 
-		function setnumkey ($numkey) {
-			$this->numkey = $numkey;
-			$tmparr = str_split(strval($numkey));
-			$numkeymap = array();
-			foreach ($tmparr as $t) $numkeymap[] = intval($t);
+	    // Wrtie output column after colum in array table, taking into account order of key
+	    $nrow = $row;
+	    $s = "";
+	    for ($i = 0; $i < $ncol; $i++)
+		for ($j = 0; $j < $nrow; $j++)
+			$s .= $table[$key[$i]][$j];
+
+	    return $s;
+	}
+
+	function decodecolumnartransposition ($msg, $key) {
+
+	    $ncol = sizeof($key);
+	    $row = 0;
+	    $col = 0;
+	    $table = array();
+
+	    // Write message column after column in array table
+	    for ($i = 0; $i < strlen($msg); $i++) {
+		$table[$key[$col]][$row] = $msg[$i];
+		if ($row < $nrow) 
+			$row++;
+		else if ($i < (strlen($msg)-1)) {
+			$row = 0;
+			$col++;
 		}
+	    }
 
-		function getnumkey () { return $this->numkey; }
+	    // Wrtie output column after colum in array table, taking into account order of key
+	    $nrow = $row;
+	    $s = "";
+	    for ($i = 0; $i < $ncol; $i++)
+		for ($j = 0; $j < $nrow; $j++)
+			$s .= $table[$key[$i]][$j];
 
-		function encode ($msg = "") {
-			// Remove everything that is not part of the alphabet
-			// Write message in 1,2,1,2,... across columns
-			// Print column after column using map
-		}
+	    return $s;
+	}
+
+
+    function getnumkey () { return $this->numkey; }
+
+    function encode ($msg = "") {
+		// Remove everything that is not part of the alphabet
+		// Write message in 1,2,1,2,... across columns
+		// Print column after column using map
+    }
     
     function decode ($msg = "") {
       // Determine if column starts with 1 or 2 characters
@@ -32,6 +89,6 @@
     }
 
 
-	}
+}
   
-  ?>
+?>
