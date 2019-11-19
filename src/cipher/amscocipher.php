a9f3900d0086194c5734c633627d9abd9d0836e4
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-19 16:32:14 +0100

    Update amscocipher.php
    Updated columnar transposition

--- src/cipher/amscocipher.php
+++ src/cipher/amscocipher.php
@@ -18,62 +18,63 @@ class amscocipher extends cipher {
 		foreach ($tmparr as $t) $numkeymap[] = intval($t);
 	}
 
-	function encodecolumnartransposition ($msg = "", $key) {
-
-	    $ncol = sizeof($key);
-	    $row = 0;
-	    $col = 0;
-	    $table = array();
-
-	    // Write message row after row in array table
-	    for ($i = 0; $i < strlen($msg); $i++) {
-		$table[$col][$row] = $msg[$i];
-		if ($col < ($ncol-1)) 
-			$col++;
-		else if ($i < (strlen($msg)-1)) {
-			$col = 0;
-			$row++;
-		}
-	    }
-
-	    // Wrtie output column after colum in array table, taking into account order of key
-	    $nrow = $row;
-	    $s = "";
-	    for ($i = 0; $i < $ncol; $i++)
-		for ($j = 0; $j < $nrow; $j++)
-			$s .= $table[$key[$i]][$j];
-
-	    return $s;
-	}
+    function encodecolumnartransposition ($msg, $key) {
+    
+        $ncol = sizeof($key);
+        $msglen = strlen($msg);
+        $nrow = ceil ($msglen / $ncol);
+        $nlongcol = $msglen % $ncol;        
+        $row = 0;
+        $col = 0;
+        $table = array();
+    
+        // Write message row after row in array table
+        for ($i = 0; $i < $msglen; $i++) {
+        	$table[$col][$row] = $msg[$i];
+        	if ($col < ($ncol-1)) 
+        		$col++;
+        	else if ($i < (strlen($msg)-1)) {
+        		$col = 0;
+        		$row++;
+        	}
+        }
+    
+        // Wrtie output column after colum in array table, taking into account order of key
+        $s = "";
+        for ($i = 0; $i < $ncol; $i++)
+            echo $i,"\t", $nrow,"\t", $nlongcol, "\n";
+        	for ($j = 0; $j < ($nrow - ($i > $nlongcol)); $j++) {
+        		$s .= $table[$key[$i]][$j];
+        		if return $s;
+    }
 
 	function decodecolumnartransposition ($msg = "", $key) {
 
 	    $ncol = sizeof($key);
-	    $nrow = ceil (strlen($msg) / $ncol);
-            $nshortcol = strlen($msg) % $ncol;
+	    $msglen = strlen($msg);
+	    $nrow = ceil ($msglen / $ncol);
+        $nlongcol = $msglen % $ncol;
 	    $col = 0;
 	    $table = array();
 
 	    // Write message column after column in array table
 	    $idx = 0;
-	    for ($i = 0; $i < $ncol; $i++) {
-		$collen = $nrow + ($nshortcol < $i);
-		for ($j = 0; $j < $collen; $j++) {
-		    $table[$key[$col]][$row] = $msg[$idx];
-		    $idx++;
-		}
+	    for ($i = 0; ($i < $ncol) && ($idx < $msglen); $i++) {
+    		$collen = $nrow + ($nlongcol < $i);
+    		for ($j = 0; ($j < $collen) && ($idx < $msglen); $j++) {
+    		    $table[$key[$i]][$j] = $msg[$idx];
+    		    $idx++;
+    		}
 	    }
 
 	    // Write output column after colum in array table, taking into account order of key
-	    $nrow = $row;
 	    $s = "";
-	    for ($i = 0; $i < $ncol; $i++)
-		for ($j = 0; $j < $nrow; $j++)
-			$s .= $table[$i][$j];
+	    for ($i = 0; ($i < $ncol) && (strlen($s) < $msglen); $i++)
+	    	for ($j = 0; ($j < $nrow) && (strlen($s) < $msglen); $j++)
+			    $s .= $table[$key[$i]][$j];
 
 	    return $s;
 	}
-
 
     	function getnumkey () { return $this->numkey; }
 
@@ -90,5 +91,15 @@ class amscocipher extends cipher {
 
 
 }
-  
+
+	
+
+$msg="thequickbrownfoxjumpsoverthelazydog";
+$key=array (2, 1, 0);
+
+$res = encodecolumnartransposition ($msg, $key);
+echo $res, "\n\n";
+$res = decodecolumnartransposition ($msg, $key);
+echo $res, "\n\n";
+
 ?>
