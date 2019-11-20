bb0816d64d6b23597e651eee12237a6a4e78891d
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-20 20:02:45 +0100

    Update ciphertoolkit.php
    Added columnar transposition generic functions

--- src/cipher/ciphertoolkit.php
+++ src/cipher/ciphertoolkit.php
@@ -172,10 +172,83 @@ abstract class cipher {
 		return $s;
 	}
 	
-	// Transposing a message reorganizes the message
-	// Transposing functions to be added	
+	// Transposing a message reorganizes the message	
 	
-    // Fractionation causes the codes for a single character to be spread across the message
+	function encodecolumnartransposition ($msgtxt, $key) {
+    
+		// Message can be a text string, a number or an array
+		// Key should be an array of integers e.g. 2,1,0 reorders columns as 0,1,2
+		$ncol = sizeof($key);
+		$msg = $this->makearray($msgtxt);
+		$msglen = sizeof($msg);
+		$nrow = ceil ($msglen / $ncol);
+		$nlongcol = $msglen % $ncol;        
+		$row = 0;
+		$col = 0;
+		$table = array();
+
+		// Write message row after row in array table
+		for ($i = 0; $i < $msglen; $i++) {
+			$table[$col][$row] = $msg[$i];
+			if ($col < ($ncol-1)) 
+				$col++;
+			else {
+				$col = 0;
+				$row++;
+			}
+		}
+
+		// Write output column after colum in array table, taking into account order of key
+		$s = "";
+		for ($i = 0; $i < $ncol; $i++) 
+			for ($j = 0; $j < ($nrow - ($key[$i] >= $nlongcol)); $j++) 
+			$s .= $table[$key[$i]][$j];
+		return $s;
+	}
+
+	function decodecolumnartransposition ($msgtxt, $key) {
+
+	    	$ncol = sizeof($key);
+		$msg  = $this->makearray($msgtxt);
+		$msglen = sizeof($msg);
+		$nrow = ceil ($msglen / $ncol);
+        	$nlongcol = $msglen % $ncol;
+	    	$table = array();
+
+		// Write message column after column in array table
+		$colidx = 0;
+		$col = array_search ($colidx++, $key);
+		$row = 0;
+		for ($i = 0; $i < $msglen; $i++) {
+			$table[$col][$row] = $msg[$i];
+			$row++;
+			if ($row >= ($nrow - ($col >= $nlongcol))) {
+				$col = array_search ($colidx++, $key);
+				$row = 0;
+			}
+		}
+        
+		// Write output row after row in array table, taking into account order of key
+		$s = "";
+		for ($i = 0; $i < $nrow; $i++) {
+		($i < ($nrow-1)) ? $rowlen = $ncol : $rowlen = $nlongcol;
+		for ($j = 0; $j < $rowlen; $j++)
+			    $s .= $table[$j][$i];
+		}
+		return $s;
+	}
+
+    	function makearray ($msg) {
+    
+		switch (gettype($msg)) {
+			case "string"   : $a = str_split($msg); break;
+			case "array"    : $a = $msg; break;
+			case "integer"  : $a = str_split (sprintf("%d", $msg)); break;
+		}
+		return $a;
+	}
+	
+        // Fractionation causes the codes for a single character to be spread across the message
 	public function fractionate ($input = "", $nrows) {
 	    
 	    //Check input
