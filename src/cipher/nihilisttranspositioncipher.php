510f81e5694c141843955ad8edf52295b3a332ea
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-28 14:48:16 +0100

    Create nihilisttranspositioncipher.php

---
+++ src/cipher/nihilisttranspositioncipher.php
@@ -0,0 +1,51 @@
+<?php
+
+  namespace cipher;
+
+  // Dummy cipher for testing no encoding / decoding implemented
+  class nihilisttranspositioncipher extends cipher {
+  
+      protected $key = "";
+      protected $keymap;
+      protected $readrow;
+      
+      public function __construct ($alphabet, $key, $readrow = TRUE) {
+        parent::__construct($alphabet);
+        $this->setkey($key);
+        $this->readrow = $readrow;
+      }
+      
+      public function setkey ($key {
+        $this->key = $key;
+        $this->size = sizeof($key);
+        $this->keymap = $this->createtranspositionkey($key);
+      }
+      
+      public function getkey () { return $this->key; }
+      public function setreadrow ($readrow = TRUE) { $this->readrow = $readrow; }
+      public function getreadrow () { return $this->readrow; }
+              
+      public function encode ($text) { 
+        // Checks
+        if ($this->key == "") return "No key specified";
+        if ($this->keymap == null) return "No key specified";
+        if ($msg == "") return "Nothing to encode";
+
+	      // Append message to fill square
+        for ($i = 0; $i < ($this->size**2 - strlen($msg)); $i++) $msg .= 'X';
+        
+        return $this->nihilisttransposition($msg, $this->keymap, $this->readrow);
+      }
+      
+      public function decode ($text) { 
+        // Checks
+        if ($this->key == "") return "No key specified";
+        if ($this->keymap == null) return "No key specified";
+        if ($msg == "") return "Nothing to encode";
+        if (strlen($msg) != $this->size**2) return "Message should be a square of the key";
+
+        return $this->nihilisttransposition($msg, $this->keymap, $this->readrow); 
+      }
+      
+  } // nihilisttranspositioncipher
+?>
