89125d9000df4c0bceaf89d25fc98d08b4c83140
KuduComp <57184611+KuduComp@users.noreply.github.com>
2019-11-28 21:36:45 +0100

    Update nihilisttranspositionTest.php
    Added test case for printing col after col

--- tests/nihilisttranspositionTest.php
+++ tests/nihilisttranspositionTest.php
@@ -11,13 +11,19 @@ class NihilisttranspositionTest extends TestCase
         // Test caesar
         $pt = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOGA";
         $ky = "213645";
-        $ct = "QTYZUHBCIGGIAMGRSQUASXEQVPHYAWAUCXG";
+        $ct = "KCBOWRHTEUIQFNOJUXTRHLAEYZDGAOPMSVEO";
         $c = new nihilisttranspositioncipher(UPPER_ALPHABET, $ky);
         $res = $c->encode ($pt);
         $this->assertEquals($ct,$res, "Error encoding nihilisttranspositioncipher");
         $res = $c->decode ($ct);
         $this->assertEquals($pt,$res, "Error decoding nihilisttranspositioncipher");
-
+        
+        // Test but with printing col after col
+        $c->setreadrow (FALSE);
+        $res = $c->encode ($pt);
+        $this->assertEquals($ct,$res, "Error encoding nihilisttranspositioncipher");
+        $res = $c->decode ($ct);
+        $this->assertEquals($pt,$res, "Error decoding nihilisttranspositioncipher");
     }
 }
 
