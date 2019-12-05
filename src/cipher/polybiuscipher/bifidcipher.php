df45f5a430a262d0e2632557c53c481fdc3c5ccb
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2019-12-05 09:46:09 +0100

    Update bifidcipher.php

--- src/cipher/polybiuscipher/bifidcipher.php
+++ src/cipher/polybiuscipher/bifidcipher.php
@@ -15,7 +15,7 @@ class bifidcipher extends \cipher\polybiuscipher {
         // Remove separators which are default added by encoding polybius
         $s=""; for ($i=0; $i<strlen($msg); $i++) if ($msg[$i] != $this->sep) $s .= $msg[$i];
         // Fractionate in two rows
-        $msg = $this->fractionate($s,2);
+        $msg = $this->rowtocolumntransposition($s,2);
         // Decode de fractionation as polybius
         $msg = parent::decode($msg);
         return $msg;
@@ -24,7 +24,7 @@ class bifidcipher extends \cipher\polybiuscipher {
     public function decode ($msg) {
         $msg = parent::encode($msg);
         $s=""; for ($i=0; $i<strlen($msg); $i++) if ($msg[$i] != $this->sep) $s .= $msg[$i];
-        $msg = $this->fractionate($s,strlen($s) / 2);
+        $msg = $this->rowtocolumntransposition($s,strlen($s) / 2);
         $msg = parent::decode($msg);
         return $msg;
     }
