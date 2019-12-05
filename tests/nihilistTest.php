f7f81c37fc150fae7d6d710fb5935bb716da1b46
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2019-12-05 14:06:32 +0100

    Update nihilistTest.php

--- tests/nihilistTest.php
+++ tests/nihilistTest.php
@@ -4,27 +4,27 @@ use PHPUnit\Framework\TestCase;
 use cipher\polybiuscipher\nihilistcipher;
 
 //All test files must be named *Test.php (case sensitive T)
-//Composer generated autoload
-
-//require_once __DIR__ . '/../vendor/autoload.php';
 
 class NihilistTest extends TestCase
 
 {
     public function testNihilist()
     {
-        $c = new nihilistcipher ("ABCDEFGHIKLMNOPQRSTUVWXYZ","ZEBRAS", "SCHAPEN");
-	$msg = "THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG";
-	$res = $c->encode ($msg);
-	$this->assertEquals("66 53 43 59 94 44 63 54 35 45 57 96 53 65 63 76 63 66 78 55 62 63 74 43 29 88 43 53 55 37 42 70 66 54 66",$res, "Error encoding nihilist");
-        $res = $c->decode ($res);
-        $this->assertEquals("THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG",$res, "Error decoding nihilist");
+	$pt = "THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG";
+	$ct = "66 53 43 59 94 44 63 54 35 45 57 96 53 65 63 76 63 66 78 55 62 63 74 43 29 88 43 53 55 37 42 70 66 54 66";
 	    
+        $c = new nihilistcipher ("ABCDEFGHIKLMNOPQRSTUVWXYZ","ZEBRAS", "SCHAPEN");
+	$res = $c->encode ($pt);
+	$this->assertEquals($ct, $res, "Error encoding nihilist");
+        $res = $c->decode ($ct);
+        $this->assertEquals($pt, $res, "Error decoding nihilist");
+	
+	$ct = "78 46 25 86 102 77 34 47 46 29 55 95 92 69 54 68 65 66 48 85 72 87 64 26 47 60 44 54 85 60 23 69 56 57 38";
 	$c->setaddkey ("KABOUTER");
 	$res = $c->encode ($msg);
-	$this->assertEquals("78 46 25 86 102 77 34 47 46 29 55 95 92 69 54 68 65 66 48 85 72 87 64 26 47 60 44 54 85 60 23 69 56 57 38",$res, "Error encoding nihilist");
-        $res = $c->decode ($res);
-        $this->assertEquals("THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG",$res, "Error decoding nihilist");
+	$this->assertEquals($ct, $res, "Error encoding nihilist");
+        $res = $c->decode ($ct);
+        $this->assertEquals($pt, $res, "Error decoding nihilist");
 	
     }
 }
