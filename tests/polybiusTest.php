684f230ad313a757d90a051417aa175eed055e06
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2019-12-05 14:28:06 +0100

    Update polybiusTest.php

--- tests/polybiusTest.php
+++ tests/polybiusTest.php
@@ -17,17 +17,17 @@ class PolybiusTest extends TestCase
 	    
         $c = new polybiuscipher (UPPER_ALPHABET_REDUCED,"12345", "12345");
 	$res = $c->encode ($pt);
-        $this->assertEquals($ct, $res, "Error encoding polybius");
+        $this->assertEquals($ct, $res, "Error encoding polybius row then column");
 	$res = $c->decode ($ct);
-        $this->assertEquals($pt, $res, "Error decoding polybius");
+        $this->assertEquals($pt, $res, "Error decoding polybius row then column");
 	    
 	$ct = "4432511454423152212443253312433542542353344315512444325113115545414322";
 	$c->sethorizontal (FALSE);
 	$res = $c->encode ($pt);
-        $this->assertEquals($ct, $res, "Error encoding polybius");
+        $this->assertEquals($ct, $res, "Error encoding polybius column then row");
         $c->setsep("");
 	$res = $c->decode ($ct);
-        $this->assertEquals($pt, $res, "Error decoding polybius");
+        $this->assertEquals($pt, $res, "Error decoding polybius column then row");
     }
 }
 
