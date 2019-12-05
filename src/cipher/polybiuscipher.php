a4359432cfdcc71673d5139aeca8819eb2f72431
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2019-12-05 10:30:31 +0100

    Update polybiuscipher.php

--- src/cipher/polybiuscipher.php
+++ src/cipher/polybiuscipher.php
@@ -8,71 +8,48 @@ class polybiuscipher extends cipher {
     protected $rows;        //Row headers
     protected $cols;        //Col headers
     protected $horizontal;  //Direction of cipher
+    protected $square;      //The polybius square
     
     public function __construct ($alphabet = UPPER_ALPHABET_REDUCED, $rows="12345", $cols="12345", $horizontal = TRUE) {
         parent::__construct ($alphabet, $rows . $cols, TRUE, 0, " ");
         $this->size = (int) sqrt (strlen($alphabet));
-        $this->rows = $rows;
-        $this->cols = $cols;
         $this->horizontal = $horizontal;
+        $this->setsquare ($rows, $cols);
     }
     
-    public function sethorizontal ($dir = TRUE) { $this->horizontal = $dir; }
+    public function sethorizontal ($dir = TRUE) { $this->horizontal = $dir; $this->setsquare($this->rows; $this->cols); }
     public function gethorizontal ($dir = TRUE) { return $this->horizontal; }
-    public function setsquare ($rows="12345", $cols="12345") { $this->rows = $rows; $this->cols = $cols; $this->validcodes = $rows . $cols; }
+    public function setsquare ($rows="12345", $cols="12345") {
+        
+        // Check ssquare
+        if ((strlen($rows) != strlen($cols)) { $this->square = null; return; }
+        if (($this->size**2) != strlen($this->alphabet)) { $this->square = null; return; }
+        
+        // Fill square
+        $this->rows = $rows; 
+        $this->cols = $cols; 
+        $this->validcodes = $rows . $cols;
+        $this->square = array();
+        for ($r = 0; $r < $this->size; $r++) $this->square[$r] = array();
+        for ($r = 0; $r < $this->size; $r++) 
+            for ($c = 0; $c < $this->size; $c++)
+                ($this->horizontal) ? $square[$this->alphabet[$r * $this->size + $c] = $rows . $cols :
+                                              $square[$this->alphabet[$r * $this->size + $c] = $cols . $rows;
+    }
     
     public function encode ($msg) {
         
-        if (($this->size**2) != strlen($this->alphabet)) return "Alphabet not a square number of characters";
-        if ((strlen($this->rows) * strlen($this->cols)) != strlen($this->alphabet)) return "Incorrect number of rows or columns";
         // Encode message
-        $s = "";
-        for ($i = 0; $i<strlen($msg); $i++) {
-            $pos = $this->strpos2 ($this->alphabet, $msg[$i]);
-            if ($pos === FALSE) {
-                if (!$this->remove) { $s .= $msg[$i]; }
-                else { return $s . "Illegal character found"; }
-            } else {
-                $idx1 = intdiv ($pos, $this->size);
-                $idx2 = $pos % $this->size;
-                ($this->horizontal) ? $s = $s . $this->rows[$idx1] . $this->cols[$idx2] . $this->sep :
-                $s = $s . $this->cols[$idx2] . $this->rows[$idx1] . $this->sep;
-            }
-        }
-        return substr($s,0,-1);
+        $this->setsep = " ";
+        return $this->arraysubstitution ($msg, $this->square);
     }
     
     public function decode ($msg) {
+        // Split msg in array of pairs assume separator
+        $msgarr = array();
+        for ($i=0; $i < strlen($msg); $i+=3) $msgarr[]=substr($msg, $i, 2);
         // Decode
-        if (($this->size**2) != strlen($this->alphabet)) return "Alphabet not a square number of characters";
-        if ((strlen($this->rows) * strlen($this->cols)) != strlen($this->alphabet)) return "Incorrect number of rows or columns";
-        $i = 0;
-        $s = "";
-        while ($i<strlen($msg)) {
-            $key = "";
-            $cnt = 0;
-            while (($cnt<2) && ($i<strlen($msg))) {
-                if ((stripos($this->rows, $msg[$i]) !== FALSE) || (stripos($this->cols, $msg[$i]) !== FALSE)) {
-                    $cnt++;
-                    $key .= $msg[$i];
-                } else
-                    if ($this->remove) { if ($msg[$i] != $this->sep) return "Illegal character found"; }
-                else $s .= $msg[$i];
-                $i++;
-            }
-            if (strlen($key) == 2) {
-                if ($this->horizontal) {
-                    $rowidx = stripos ($this->rows, $key[0]);
-                    $colidx = stripos ($this->cols, $key[1]);
-                    $s .= $this->alphabet[$rowidx * $this->size + $colidx];
-                } else {
-                    $colidx = stripos ($this->cols, $key[0]);
-                    $rowidx = stripos ($this->rows, $key[1]);
-                    $s .= $this->alphabet[$colidx * $this->size + $rowidx];
-                };
-            }
-        }
-        return $s;
+        return $this->arraysubstitution ($msgarr, array_flip($this->square));
     }
     
 } // End of polybiuscipher
