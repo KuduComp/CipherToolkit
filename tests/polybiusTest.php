7f441f55b91a56431f90ef2b60dd1b31bf780bd3
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2019-12-05 13:55:40 +0100

    Update polybiusTest.php

--- tests/polybiusTest.php
+++ tests/polybiusTest.php
@@ -13,9 +13,11 @@ class PolybiusTest extends TestCase
     public function testPolybius()
     {
         $c = new polybiuscipher (UPPER_ALPHABET_REDUCED,"12345", "12345");
-		$res = $c->encode ("THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG");
+	$c->setsep(" ");
+	$res = $c->encode ("THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG");
         $this->assertEquals("44 23 15 41 45 24 13 25 12 42 34 52 33 21 34 53 24 45 32 35 43 34 51 15 42 44 23 15 31 11 55 54 14 34 22",$res, "Error encoding ADFGX");
-        $res = $c->decode ($res);
+        $c->setsetp("");
+	$res = $c->decode ($res);
         $this->assertEquals("THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG",$res, "Error decoding ADFGX");
     }
 }
