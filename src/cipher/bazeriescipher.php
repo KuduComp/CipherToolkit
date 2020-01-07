a8a085bb68b6867f37d422c268f695a6408a0785
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2020-01-07 16:05:37 +0100

    Create bazeriescipher.php
    New cipher added bazeries

---
+++ src/cipher/bazeriescipher.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace cipher;
+
+// Bazeries cipher
+// Uses two squares
+// Locate character in 1st square and replaces with the 2nd square
+
+class bazeriescipher extends cipher {
+
+	protected $alphabet;
+	protected $key1;	// Sets square 1
+	protected $key2;	// Sets square 2, typically equals n written out
+	protected $sq1, $sq2;
+	protected $n; 		// Determines grouping
+	protected $chunk;
+
+	public function __construct ($alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ", $key1 = "", $key2 = "", $n = 1) {
+		parent::__construct ($alphabet);
+		$this->setkeys ($key1, $key2);
+		$this->setn ($n);
+	}
+
+	public function setkeys ($key1 = "", $key2 = "") {
+		$this->key1 = $key1;
+		$this->sq1  = $this->shufflealphabet ($this->alphabet, $key1);
+		$this->key2 = $key2;
+		$this->sq2  = $this->shufflealphabet ($this->alphabet, $key2);
+	}
+
+	public function setsquares ($sq1, $sq2) {
+		// Set squares directly
+		$this->key1 = "Unused";
+		$this->key2 = "Unused";
+		$this->sq1  = $sq1;
+		$this->sq2  = $sq2;
+	}
+
+	public function setn ($n = 1) {
+		$this->n = $n;
+		$this->chunk = str_split ( (string) $n); 
+	}
+
+	public function getkeys () { return array($this->key1, $this->key2); }
+	public function getsquares() { return array ($this->sq1, $this->sq2); }
+	public function getn() { return $this->n; }
+
+	public function encode ($msg) {
+
+		// Remove spaces and other stuff
+		$msg = $this->cleaninput ($msg);
+
+		// Split msg in chunks and reverse each chunk
+		$s = "";
+		$i = 0;
+		$len = sizeof ($this->chunk);
+		while ($msg != "") {
+			$s .= strrev (substr($msg, 0, $this->chunk[$i % $len]));
+			$msg = substr($msg, $this->chunk[$i % $len]);
+			$i++;
+		}
+
+		// Code the string
+		$s2 = "";
+		for ($i = 0; $i < strlen ($s); $i++) {
+			$pos = stripos ($this->sq1, $s[$i]);
+			if ($pos !== FALSE) $s2 .= $this->sq2[$pos];
+		}
+		return $s2;
+	}
+
+	public function decode ($msg) {
+
+		// Remove spaces and other stuff
+		$msg = $this->cleaninput ($msg);
+
+		// Decode the string
+		$s = "";
+		for ($i = 0; $i < strlen ($msg); $i++) {
+			$pos = stripos ($this->sq2, $msg[$i]);
+			if ($pos !== FALSE) $s .= $this->sq1[$pos];
+		}
+
+		// Reverse chunks
+		$s2 = "";
+		$i = 0;
+		$len = sizeof ($this->chunk);
+		while ($s != "") {
+			$s2 .= strrev (substr($s, 0, $this->chunk[$i % $len]));
+			$s = substr($s, $this->chunk[$i % $len]);
+			$i++;
+		}
+
+		return $s2;
+	}
+
+} // bazeriescipher
+
+?>
