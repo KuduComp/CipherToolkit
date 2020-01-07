eaa2f87905a8c0626ffe4096b98820e9670d2072
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2020-01-07 08:13:49 +0100

    Create phillipsTest.php

---
+++ tests/phillipsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use cipher\phillipscipher;
+use const \cipher\UPPER_ALPHABET_REDUCED;
+
+class PhillipsTest extends TestCase
+{
+    public function testPhillips()
+    {
+        // Test condicipher
+        $pt = "THETHINGSTHATCOMETOTHOSEWHOWAITMAYBETHETHINGSLEFTBYTHOSEWHOGOTTHEREFURST");
+		    $ct = "DRNDRMAQZLTRSKWOVYAYRWZNTBWTKMLOKEYGLRGLRFHQZUGPLIFSTWZVBRAQWDDRNYGOMYFL";
+        $c = new phillipscipher(UPPER_ALPHABET_REDUCED, "PATIENCE");
+        $res = $c->encode ($pt);
+        $this->assertEquals($ct,$res, "Error encoding phillipscipher");
+        $res = $c->decode ($ct);
+        $this->assertEquals($pt,$res, "Error decoding phillipscipher");
+                 
+    }
+}
+
+?>
