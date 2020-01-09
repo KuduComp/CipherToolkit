0972d36baf686649220b34320294b090fa1a4373
kudu-comp <57184611+kudu-comp@users.noreply.github.com>
2020-01-09 17:15:43 +0100

    Update bazeriesTest.php

--- tests/bazeriesTest.php
+++ tests/bazeriesTest.php
@@ -8,28 +8,26 @@ class BazeriesTest extends TestCase
 {
     public function testBazeries()
     {
-        // Test condicipher
-        $pt = "SIMPLESUBSTITUTIONPLUSTRANSPOSITION";
-        $ct = "ACYYUXYMRQKXKCKGCRQIYITNKYXKCYGQGCI";
-        
+        // Test bazeries        
         $c = new bazeriescipher(UPPER_ALPHABET_REDUCED, "", "", 3752);
+       
+        // Test default with square 1 vertical and square 2 uses spelled out n
 	    $pt = "THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG";
 		$ct = "MTYAVKRHDWISSLNRPXGYSOHVBMTCEVUZFSQ";
+        $res = $c->encode ($pt);
+        $this->assertEquals($ct, $res, "Error encoding bazeriescipher default");
+        $res = $c->decode ($ct);
+        $this->assertEquals($pt, $res, "Error decoding bazeriescipher default"); 
         
         // Test manually set square and n
+		$ct = "MTYAVKRHDWISSLNRPXGYSOHVBMTCEVUZFSQ";
         $c->setsquares ("AFLQVBGMRWCHNSXDIOTYEKPUZ", "CLEABDFGHIKMNOPQRSTUVWXYZ");
         $c->setn (23);
         
         $res = $c->encode ($pt);
-        $this->assertEquals($ct,$res, "Error encoding bazeriescipher squares set");
+        $this->assertEquals($ct, $res, "Error encoding bazeriescipher squares set");
         $res = $c->decode ($ct);
-        $this->assertEquals($pt,$res, "Error decoding bazeriescipher squares set");
-        
-        // Test default with square 1 vertical and square 2 uses spelled out n
-        $res = $c->encode ($pt);
-        $this->assertEquals($ct,$res, "Error encoding bazeriescipher default");
-        $res = $c->decode ($ct);
-        $this->assertEquals($pt,$res, "Error decoding bazeriescipher default"); 
+        $this->assertEquals($pt, $res, "Error decoding bazeriescipher squares set");
         
     }
 }
