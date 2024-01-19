0cc60fb8dd2fc191f15fe5cf14cad0ff11a51c2c
Aernout <akoolen@quicknet.nl>
2024-01-19 21:12:20 +0100

    Updated testcases

--- tests/adfgvxTest.php
+++ tests/adfgvxTest.php
@@ -9,7 +9,7 @@ class ADFGVXTest extends TestCase
 {
     public function testADFGVX()
     {
-        $c = new ADFGVXcipher ("ABCDEFGHIKLMNOPQRSTUVWXYZ","PILOTEN");
+        $c = new ADFGVXcipher ("ABCDEFGHIKLMNOPQRSTUVWXYZ","", "PILOTEN");
 		$c->addreplacement ("j","i");
 		$msg = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG";
 		$msg = $c->cleaninput ($msg);
@@ -18,7 +18,7 @@ class ADFGVXTest extends TestCase
 		$this->assertEquals("XADDGGXAXDGGXXGFGGAADXADXDXGAGGFFAGFGXXDFDDFFFADAFGADGFXFDFGAGGFDXAFXG",$res, "Error encoding ADFGX");
         $res = $c->decode ($res);
         $this->assertEquals("THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG",$res, "Error decoding ADFGX");
-        $c = new ADFGVXcipher ("A1B2C3D4E5F6G7H8I9J0KLMNOPQRSTUVWXYZ","PILOTEN");
+        $c = new ADFGVXcipher ("A1B2C3D4E5F6G7H8I9J0KLMNOPQRSTUVWXYZ", "", "PILOTEN");
         $res = $c->encode ("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG123");
         $this->assertEquals("FAGDAVFDXFXXXFXAGAVGDDFAAFXVXXAAAVVVVXVVFXAFFFGGVDFAVGVFGAVAVGGVADVVXGDDFXAA",$res, "Error encoding ADFGVX");
         $res = $c->decode ($res);
--- tests/polybiusTest.php
+++ tests/polybiusTest.php
@@ -14,20 +14,14 @@ class PolybiusTest extends TestCase
     {
 	$pt = "THEQUICKBROWNFOXIUMPSOVERTHELAZYDOG";
 	$ct = "4423154145241325124234523321345324453235433451154244231531115554143422";
+        $key = "";
 	    
-        $c = new polybiuscipher (UPPER_ALPHABET_REDUCED,"12345", "12345");
+        $c = new polybiuscipher (UPPER_ALPHABET_REDUCED, $key, "12345", "12345");
 	$res = $c->encode ($pt);
         $this->assertEquals($ct, $res, "Error encoding polybius row then column");
 	$res = $c->decode ($ct);
         $this->assertEquals($pt, $res, "Error decoding polybius row then column");
 	    
-	$ct = "4432511454423152212443253312433542542353344315512444325113115545414322";
-	$c->sethorizontal (FALSE);
-	$res = $c->encode ($pt);
-        $this->assertEquals($ct, $res, "Error encoding polybius column then row");
-        $c->setsep("");
-	$res = $c->decode ($ct);
-        $this->assertEquals($pt, $res, "Error decoding polybius column then row");
     }
 }
 
